fb7c7f351c6e70e56c65963670454b307d06bd4d
Miskatur <Miskaturrahman34826@gmail.com>
2022-08-23 00:45:13 +0600

    select part aadded

---
+++ js/selection.js
@@ -0,0 +1,43 @@
+const playerNameArray = [];
+
+function playerNameDisplay(playerNameArray) {
+    const playersNameOrderList = document.getElementById('playersNameOrderList');
+    playersNameOrderList.innerHTML = '';
+
+    for (let i = 0; i < playerNameArray.length; i++) {
+
+
+        // playerNameArray.length.pop();
+
+        const playerName = playerNameArray[i].playerName;
+        const playerList = document.createElement('li');
+        playerList.innerText = playerName;
+        playersNameOrderList.appendChild(playerList);
+
+
+
+
+    }
+}
+
+function addToUl(element) {
+    // console.log(element.parentNode.parentNode.children);
+    // console.log(element.parentNode.parentNode.children[0].innerText);
+    const playerName = element.parentNode.parentNode.children[0].innerText;
+    const playerNameObject = {
+        playerName: playerName
+    }
+    playerNameArray.push(playerNameObject)
+    if (playerNameArray.length > 5) {
+        alert("You can't select more than 5 player")
+        return
+    }
+    else {
+        document.getElementById('selected-players').innerText = playerNameArray.length;
+        playerNameDisplay(playerNameArray)
+    }
+
+
+
+
+}
