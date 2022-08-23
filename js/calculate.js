461dba58f68f545bb36ee6f107c0cc312066180c
Miskatur <Miskaturrahman34826@gmail.com>
2022-08-23 14:38:08 +0600

    input field validation added

--- js/calculate.js
+++ js/calculate.js
@@ -3,22 +3,33 @@ function getInputFieldElementById(elementId) {
     const inputString = inputElement.value;
     const input = parseFloat(inputString);
     return input;
+
 }
 
 function getTextElementById(elementId) {
     const textElement = document.getElementById(elementId);
+
     const textElementString = textElement.innerText;
     const text = parseFloat(textElementString);
+
     return text;
 }
 
 function setTextElementById(elementId, value) {
     const newTextElement = document.getElementById(elementId);
+
     newTextElement.innerText = value;
+
 }
+
+
 
 document.getElementById('btn-calculate').addEventListener('click', function () {
     const perPlayerField = getInputFieldElementById('per-player-field')
+    if (isNaN(perPlayerField)) {
+        alert('Please Input a Number.')
+        return
+    }
 
     const totalPlayerCost = perPlayerField * (playerNameArray.length);
     setTextElementById('player-expenses', totalPlayerCost)
@@ -27,9 +38,22 @@ document.getElementById('btn-calculate').addEventListener('click', function () {
 
 document.getElementById('btn-total').addEventListener('click', function () {
     const perPlayerField = getInputFieldElementById('per-player-field')
+    if (isNaN(perPlayerField)) {
+        alert('Please Input a Number.')
+        return
+    }
+
     const totalPlayerCost = perPlayerField * (playerNameArray.length);
     const managerCost = getInputFieldElementById('manager-field');
+    if (isNaN(managerCost)) {
+        alert('Please Input a Number.')
+        return
+    }
     const coachCost = getInputFieldElementById('coach-field');
+    if (isNaN(coachCost)) {
+        alert('Please Input a Number.')
+        return
+    }
 
     const totalCost = totalPlayerCost + managerCost + coachCost;
 
